58f62ce3ee3b78685c5e3cbc3bde2dddce1d769e
Mobile Perpetuum <perpetuum-mobile@ukr.net>
2018-10-11 20:51:13 +0300

    More informative view

--- spec/features/chrome_spec.rb
+++ spec/features/chrome_spec.rb
@@ -5,6 +5,7 @@ describe 'chrome fail' do
     visit '/input'
     %w[left_number right_number].each do |id|
       element = find("##{id}")
+      element.click_at()
       5.times do
         page.driver.browser.action.move_to(element.native, 145, 5)
                                   .click
@@ -13,9 +14,11 @@ describe 'chrome fail' do
                                   .click
                                   .click
                                   .perform
+        sleep 0.5
       end
 
       page.save_and_open_screenshot("#{id}.png")
     end
+    sleep 5
   end
 end
